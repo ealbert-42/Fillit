0a4dfb583d3b7530a28720b74f568cd2b15b52b0
Eddy ALBERT <ealbert@e2r11p5.42.fr>
2016-01-12 15:43:27 +0100

    modif 12 janvier : enregistrer les tetris + remplacer les diez

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ealbert <ealbert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/22 15:20:24 by ealbert           #+#    #+#             */
-/*   Updated: 2015/12/28 20:51:52 by ealbert          ###   ########.fr       */
+/*   Updated: 2016/01/12 15:42:49 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		ft_error(void)
 
 /* Apres la resolution, on affiche la map */
 
-void	print_map(char /* * ou ** ?*/str)
+void		print_map(char /* * ou ** ?*/str)
 {
 /* Quand le probleme de la map sera regle, on pourra attaquer cette partie
  */
@@ -55,48 +55,99 @@ t_triminos	*up_and_left(t_triminos *t1)
  */
 }
 
-/* Creation d'un maillon pour enregistrer le tetriminos */
+char		*replace_diez(char *str, int letter)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '#')
+			str[i] = letter;
+		i++;
+	}
+	return (str);
+}
 
 t_triminos	*add_tetri(t_triminos *t1, char *str, int letter)
 {
-/* Assez facile, mais un peu chiant quand meme
- * malloc le pointeur sur structure
- * piece = str
- * letter = A pour le premier, B pour le deuxieme etc...
- * left = NULL si c'est le premier, le precedent tetriminos sinon
- * right = NULL, quoiqu'il arrive
- */
+	t_triminos	*t2;
+
+	if (!(t2 = (t_triminos *)malloc(t_triminos)))
+		ft_error();
+	if (!(t2->piece = (char *)malloc(sizeof(char) * 20)))
+		ft_error();
+	t2->piece = ft_strcpy(t1->piece, str);
+	t2->piece = replace_diez(t2->piece, letter);
+	t2->letter = letter;
+	t2->left = t1;
+	t2->right = NULL;
+	return (t2);
+}
+
+t_triminos	*init_tetri(t_triminos *t1, char *str)
+{
+	if (!(t1->piece = (char *)malloc(sizeof(char) * 20)))
+		ft_error();
+	t1->piece = ft_strcpy(t1->piece, str);
+	t1->letter = 65;
+	t1->piece = replace_diez(t1->piece, letter);
+	t1->left = NULL;
+	t1->right = NULL;
+	return (t1);
 }
 
 /* checker le fichier, savoir s'il est valide */
 
-int		valid_string(char *str)
+int			valid_string(char *str)
 {
-/* Check si str fais bien le schema suivant :
- * '.#..\n'
- * '.#..\n'
- * '.##.\n'
- * '....\n'
- * '\n' Ou un truc du genre.
- */
+	int		i;
+	int		nbdiez;
+	int		nbchar;
+
+	nbchar = 0;
+	nbdiez = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (nbdiez < 5 && str[i] == '#')
+		{
+			if (str[i + 1] == '#' || str[i + 5] == '#')
+				nbdiez++;
+			else
+				ft_error();
+		}
+		else if (nbdiez >= 5 || str[i] != '.' || str[i] != '\n')
+			ft_error();
+		i++;
+		nbchar++;
+		if (nbchar == 4 && str[i] != '\n')
+			ft_error();
+	}
+	return (1);
 }
 
 /* Fonction pour lire le fichier d'entree */
 
 t_triminos	*read_file(int fd)
 {
-	char	buf[BUFFER_SIZE];
-	int		ret;
-	int		nbtetri;
-	t_triminos *t1;
+	char		buf[BUFFER_SIZE];
+	int			ret;
+	int			nbtetri;
+	t_triminos	*t1;
 
 	nbtetri = 0;
-	t1 = NULL;
+	if (!(t1 = (t_triminos *)malloc(t_triminos)))
+		return (NULL);
 	while (nbtetri < 26 || (ret = read(fd, &buf, BUFFER_SIZE)) < 21)
 	{
 		buf[ret] = '\0';
-		if (valid_string(buf) == 1)
-			piece = add_tetri(t1, buf, (LETTER + nbtetri));
+		if (valid_string(buf) == 1 && nbtetri == 0)
+			t1 = init_tetri(t1, buf);
+		else if (valid_string(buf) == 1)
+			t1 = add_tetri(t1, buf, (LETTER + nbtetri));
+		else
+			ft_error();
 		nbtetri++;
 	}
 	ret = read(fd, &buf, BUFFER_SIZE);
@@ -110,6 +161,6 @@ int			main(int argc, char **argv)
 	int		fd;
 
 	if ((fd = open(argv[1], O_RDONLY)) < 0 || argc != 2)
-			ft_error();
+		ft_error();
 	return (0);
 }
