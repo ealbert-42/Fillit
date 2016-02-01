66e175d5e66981cc0850e37988c5c8b5a7fe1a2b
ealbert-42 <eddy94250@hotmail.fr>
2016-02-01 17:30:15 +0100

    Map_limit added

--- srcs/main.c
+++ srcs/main.c
@@ -17,13 +17,16 @@ int		main(int argc, char **argv)
 	int			fd;
 	t_triminos	*t;
 	char 		**map;
-
+	int			map_limit;
+	
 	if (argc != 2)
 		ft_error(0);
 	if ((fd = open(argv[1], O_RDONLY)) < 0)
 		ft_error(1);
-	t = ft_read_file(fd); //read and save trimmed tetri to list and return pointer to beginlist
+	t = ft_read_file(fd);
+	close(fd)//read and save trimmed tetri to list and return pointer to beginlist
 	map = ft_generate_map();
+	map_limit = map_init(t);
 	if (ft_algo(map, t) == 1)
 		ft_print_map(map);
 	else
