2360c952616d683365fe7345e5b628ef312b1187
Eddy ALBERT <ealbert@e1r1p6.42.fr>
2016-02-07 18:31:11 +0100

    one final effort

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ealbert <ealbert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 15:40:42 by ealbert           #+#    #+#             */
-/*   Updated: 2016/02/07 15:16:20 by ealbert          ###   ########.fr       */
+/*   Updated: 2016/02/07 18:27:16 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int			main(int argc, char **argv)
 	{
 		if (ft_algo(map, t) == 1)
 		{
-			ft_putendl("Success");
 			ft_print_map(map->map, map->maxsize);
 			return (0);
 		}
