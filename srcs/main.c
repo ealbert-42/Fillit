8f4919d3a47d089b4094c54e1623b4aed30563c7
Eddy ALBERT <ealbert@e1r1p6.42.fr>
2016-02-07 15:20:51 +0100

    ca galere severe

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,28 @@
 /*   By: ealbert <ealbert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 15:40:42 by ealbert           #+#    #+#             */
-/*   Updated: 2016/02/02 16:27:54 by ealbert          ###   ########.fr       */
+/*   Updated: 2016/02/07 15:16:20 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void	ft_print_map(char **map)
+static void	ft_print_map(char **map, int maxsize)
 {
 	int		i;
+	int		j;
 
 	i = 0;
-	while (map[i])
+	while (i <= maxsize)
 	{
-		ft_putendl(map[i]);
+		while (j <= maxsize)
+		{
+			ft_putchar(map[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
 		i++;
+		j = 0;
 	}
 }
 
@@ -42,12 +49,11 @@ int			main(int argc, char **argv)
 		if (ft_algo(map, t) == 1)
 		{
 			ft_putendl("Success");
-			ft_print_map(map->map);
+			ft_print_map(map->map, map->maxsize);
 			return (0);
 		}
 		map->maxsize++;
 	}
 	ft_error(4);
-	ft_print_map(map->map);
 	return (0);
 }
